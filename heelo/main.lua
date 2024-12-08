5cab905f36a7713b80f4183ff6012c2423c0a625
troy-beep <krish.disbound@gmail.com>
2024-12-08 18:17:40 +0000

    Update main.lua

--- heelo/main.lua
+++ heelo/main.lua
@@ -3,7 +3,7 @@ local pretty = require(".pretty")
 local formatted = pretty.tprint({
   name = "Jhony Sins",
   age = 22,
-  friends = { "Dani", "Angelina","Valentina","Mia","Adrina" }
+  friends = { "Dani", "Angelina","Valentina","Mia","Adrina","Giana"}
 }, 2)
 
 -- prints the formatted table structure
